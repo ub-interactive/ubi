a09417a5837136a5f16080d3f1c9363c54e28aae
83225506 <83225506@qq.com>
2020-03-13 17:38:28 +0800

    update

--- ubi-ccat/src/main/scala/com/ubi/ccat/controllers/ApplicationController.scala
+++ ubi-ccat/src/main/scala/com/ubi/ccat/controllers/ApplicationController.scala
@@ -2,27 +2,30 @@ package com.ubi.ccat.controllers
 
 import com.ubi.ccat.controllers.api.web.WebApiController
 import javax.inject.Inject
+import play.api.Environment
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.mvc.{Action, AnyContent}
 
 import scala.concurrent.ExecutionContext
 
 class ApplicationController @Inject()(
+  environment: Environment,
   val dbConfigProvider: DatabaseConfigProvider
 )
   (implicit val ec: ExecutionContext) extends WebApiController {
 
   def index: Action[AnyContent] = {
-    Action.async { implicit request =>
-      import com.ubi.ccat.persistence.slick.Tables._
-      import com.ubi.ccat.persistence.slick.Tables.profile.api._
+    Action {
+      Ok
+    }
+  }
 
-      for {
-        _ <- db.run(SubjectRepository.rows.schema.createIfNotExists)
-        _ <- db.run(CourseSubjectRepository.rows.schema.createIfNotExists)
-      } yield Ok
-
-
+  def file(path: String): Action[AnyContent] = {
+    Action { implicit request =>
+      environment.getExistingFile(s"/uploaded/$path") match {
+        case Some(value) => Ok.sendFile(value)
+        case None => NotFound
+      }
     }
   }
 }
