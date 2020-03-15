f08c80f0bd7c403716b90c25e299a4abb2f61615
83225506 <83225506@qq.com>
2020-03-16 04:45:09 +0800

    update

---
+++ ubi-ccat/src/main/resources/evolutions/default/5.sql
@@ -0,0 +1,16 @@
+# --- !Ups
+create table ubi_ccat.order
+(
+    order_id     binary(16)   not null primary key,
+    course_id    binary(16)   not null,
+    order_no     varchar(255) not null,
+    order_status varchar(255) not null,
+    created_at   text         not null,
+    payment_at   text         not null,
+    price        int          not null,
+    quantity     int          not null,
+    total_amount int          not null
+);
+
+# --- !Downs
+drop table if exists ubi_ccat.order;
