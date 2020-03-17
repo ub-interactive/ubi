c1acaeea7751ef4d248be23fde618a15d779d087
83225506 <83225506@qq.com>
2020-03-18 03:06:00 +0800

    update

--- ubi-ccat/src/main/scala/com/ubi/ccat/configs/AliCloudServiceConfig.scala
+++ ubi-ccat/src/main/scala/com/ubi/ccat/configs/AliCloudServiceConfig.scala
@@ -4,17 +4,21 @@ import com.typesafe.config.Config
 import play.api.{ConfigLoader, Configuration}
 
 case class AliCloudServiceConfig(
+  regionId: String,
   accessKey: String,
-  accessSecret: String
+  accessSecret: String,
+  signName: String
 )
 
 object AliCloudServiceConfig {
   implicit val configLoader: ConfigLoader[AliCloudServiceConfig] =
-    (config: Config, path: String) ⇒ {
+    (config: Config, path: String) => {
       val c = Configuration(config.getConfig(path))
       AliCloudServiceConfig(
+        regionId = c.get[String]("region-id"),
         accessKey = c.get[String]("access-key"),
-        accessSecret = c.get[String]("access-secret")
+        accessSecret = c.get[String]("access-secret"),
+        signName = c.get[String]("sign-name")
       )
     }
 }
--- ubi-ccat/src/main/scala/com/ubi/ccat/services/ShortMessageService.scala
+++ ubi-ccat/src/main/scala/com/ubi/ccat/services/ShortMessageService.scala
@@ -1,30 +1,71 @@
 package com.ubi.ccat.services
 
-class ShortMessageService {
+import com.aliyuncs.http.MethodType
+import com.aliyuncs.profile.DefaultProfile
+import com.aliyuncs.{CommonRequest, DefaultAcsClient}
+import com.ubi.ccat.configs.AliCloudServiceConfig
+import com.ubi.ccat.services.ShortMessageService.SmsMessageResponse
+import javax.inject.Inject
+import play.api.libs.json.{Json, OFormat}
 
-  def sendSms() = {
+import scala.util.Try
 
-//    DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
-//    IAcsClient client = new DefaultAcsClient(profile);
-//
-//    CommonRequest request = new CommonRequest();
-//    request.setSysMethod(MethodType.POST);
-//    request.setSysDomain("dysmsapi.aliyuncs.com");
-//    request.setSysVersion("2017-05-25");
-//    request.setSysAction("SendSms");
-//    request.putQueryParameter("RegionId", "cn-hangzhou");
-//    request.putQueryParameter("PhoneNumbers", "18600094776");
-//    request.putQueryParameter("SignName", "signname");
-//    request.putQueryParameter("TemplateCode", "templatecode");
-//    try {
-//      CommonResponse response = client.getCommonResponse(request);
-//      System.out.println(response.getData());
-//    } catch (ServerException e) {
-//      e.printStackTrace();
-//    } catch (ClientException e) {
-//      e.printStackTrace();
-//    }
+class ShortMessageService @Inject()(aliCloudServiceConfig: AliCloudServiceConfig) {
 
-    ???
+  val profile: DefaultProfile = DefaultProfile.getProfile(aliCloudServiceConfig.regionId, aliCloudServiceConfig.accessKey, aliCloudServiceConfig.accessSecret)
+  val client: DefaultAcsClient = new DefaultAcsClient(profile)
+
+  private def withRequest(process: CommonRequest => CommonRequest): Try[SmsMessageResponse] = {
+    val request = new CommonRequest()
+    request.setMethod(MethodType.POST)
+    request.setDomain("dysmsapi.aliyuncs.com")
+    request.setVersion("2017-05-25")
+    request.setAction("SendSms")
+    request.putQueryParameter("RegionId", aliCloudServiceConfig.regionId)
+    request.putQueryParameter("SignName", aliCloudServiceConfig.signName)
+
+    Try(Json.parse(client.getCommonResponse(process(request)).getData).validate[SmsMessageResponse].get)
   }
+
+
+  def sendSms(message: ShortMessageService.SmsMessage): Try[String] = {
+    withRequest { request =>
+      request.putQueryParameter("PhoneNumbers", message.phoneNumbers.mkString(","))
+      request.putQueryParameter("TemplateCode", message.templateCode)
+      request.putQueryParameter("TemplateParam", Json.toJson(message.templateParam).toString())
+      request
+    }.map(_.BizId)
+  }
+}
+
+object ShortMessageService {
+
+  sealed trait SmsMessage {
+    def phoneNumbers: Seq[String]
+
+    def templateCode: String
+
+    def templateParam: Map[String, String]
+  }
+
+  final case class MobileVerifyMessage(
+    phoneNumber: String,
+    code: String
+  ) extends SmsMessage {
+    val templateCode = "SMS_186395028"
+    val phoneNumbers = Seq(phoneNumber)
+    val templateParam = Map("code" -> code)
+  }
+
+  final case class SmsMessageResponse(
+    Message: String,
+    RequestId: String,
+    BizId: String,
+    Code: String
+  )
+
+  object SmsMessageResponse {
+    implicit val format: OFormat[SmsMessageResponse] = Json.format[SmsMessageResponse]
+  }
+
 }
